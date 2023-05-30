4d6ab07eb05ccbe0cd433ccea81ebfa22f2fca85
Bo Wood <bo.wood@zxsecurity.co.nz>
2023-05-30 16:21:11 +1200

    status code filtering partially functional

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 	"time"
 
@@ -46,12 +47,12 @@ pierre v1.0	                                        @cicada-legs
 	
 ▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪
 
-❖   Url 				%s
+❖   URL 				%s
 ❖   Method 				%s
 ❖   Wordlist 				%s
 ❖   Extensions 				%s
 ❖   Threads (Goroutines) 		%d
-❖   Timeout 				%d
+❖   Timeout (seconds) 			%d
 
 ▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪▪
 
@@ -75,24 +76,27 @@ func parse_flags(scan *scan_config) {
 	flag.StringVar(&scan.wordlist_file, "w", "", "provide a wordlist")         //change later??
 	flag.StringVar(&scan.url, "u", "", "target url")
 	flag.StringVar(&scan.extension, "x", "", "list of extensions to fuzz with: comma separated")
-	flag.IntVar(&scan.threads, "t", 1, "specify the number of threads to run on")
-	flag.IntVar(&scan.timeout, "c", 100, "specify the limit for when requests should timeout")
+	flag.IntVar(&scan.threads, "th", 1, "specify the number of threads to run on")
+	flag.IntVar(&scan.timeout, "to", 100, "specify the limit for when requests should timeout")
+	flag.StringVar(&scan.filter_codes_string, "fs", "200,204,301,302,307,400,401,403,404,405,500", "specify which status codes to be included in output") //TODO: add more codes!!!
 
 	flag.Parse()
 }
 
 type scan_config struct {
 	//
-	post          bool
-	url           string
-	wordlist_file string
-	extension     string
-	threads       int
-	timeout       int
+	post                bool
+	url                 string
+	wordlist_file       string
+	extension           string
+	threads             int
+	timeout             int
+	filter_codes_string string
 }
 
 func (s scan_config) fuzz_scan() { //post by default
 
+	output := ""
 	ext_slice := strings.Split(s.extension, ",") // TODO: put this somewhere better
 
 	fileread, err := os.Open(s.wordlist_file)
@@ -110,15 +114,10 @@ func (s scan_config) fuzz_scan() { //post by default
 
 			for i := 0; i < len(ext_slice); i++ {
 
-				// ctx, cancel := context.WithTimeout(context.Background(), s.timeout*time.Second)
-				// defer cancel()
-
-				// req, err := http.NewRequestWithContext(ctx, "GET", s.url+scanner.Text()+ext_slice[i], nil)
-
 				client := &http.Client{
 					Timeout: time.Duration(s.timeout) * time.Millisecond,
 				}
-				req, err := http.NewRequest("GET", s.url+scanner.Text()+ext_slice[1], nil) // client.Get(s.url + scanner.Text() + ext_slice[1])
+				req, err := http.NewRequest("GET", s.url+scanner.Text()+ext_slice[i], nil) // client.Get(s.url + scanner.Text() + ext_slice[1])
 
 				if err != nil {
 					handle_errors(err, "getettet")
@@ -126,38 +125,31 @@ func (s scan_config) fuzz_scan() { //post by default
 				}
 				resp, err := client.Do(req)
 				if err != nil {
-					fmt.Println(err)
+					// fmt.Println(err) /this is for testing only
+					//connection timeout, what to output at the end of the scan?
 					continue
 				}
 
-				//handle_errors(err, "second ewrrorerko")
-
-				// client := http.Client{Timeout: s.timeout * time.Second} //TODO: change this to be the correct thing until im ready
-				// fmt.Println("timeout test: ", s.timeout)
-				// resp, err := client.Get(s.url + scanner.Text() + ext_slice[i])
-				// if os.IsTimeout(err) {
-				// 	handle_errors(err, "request timeout") //for when the request itself times out
-				// 	continue                              //go to the next loop iteraion
-				// } else if err != nil {
-				// 	handle_errors(err, "get error") // for when the host isnt up/cant be connected to
-				// // }
 				defer resp.Body.Close()
-				fmt.Println("/"+scanner.Text()+ext_slice[i]+"\t\t\t[ Status:", resp.StatusCode, " Size:", resp.ContentLength, "]")
-				//fmt.Printf("/%s%s\t\t\t[ Status: %d  Size:%d ]\n", scanner.Text(), ext_slice[i], resp.StatusCode, resp.ContentLength)
+				// fmt.Println("code", resp.StatusCode)
+				if strings.Contains(s.filter_codes_string, strconv.Itoa(resp.StatusCode)) { //add to output if matching code
+					output += output + "/" + scanner.Text() + ext_slice[i] + "\t\t\t[ Status: " + strconv.Itoa(resp.StatusCode) + " Size: " + strconv.FormatInt(resp.ContentLength, 10) + " ]\n"
+					// fmt.Printf("/%s%s\t\t\t[ Status: %d  Size:%d ]\n", scanner.Text(), ext_slice[i], resp.StatusCode, resp.ContentLength)
+					//fmt.Println(output)
+
+				}
+
 			}
 			fileread.Close()
 		}
-	}
 
+	}
+	fmt.Println(output)
 }
 
 func handle_errors(err error, msg string) {
 	if err != nil {
-		fmt.Println(msg)
+		fmt.Printf(msg)
 		os.Exit(1) //change this later to be different for different errors
 	}
-}
-
-func fuzz() {
-	//
 }
