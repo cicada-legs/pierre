92b0535af91b31202598b5d6bc8436dc870c0740
Bo Wood <bo.wood@zxsecurity.co.nz>
2023-06-19 16:31:50 +1200

    fixed bug with repetitive prints

--- main.go
+++ main.go
@@ -15,6 +15,12 @@ import (
 )
 
 /*
+	example command:
+	go run main.go -u https://www.google.com -w wordlist.txt -x php,html,txt -th 10 -to 100 -fs 200,204,301,302,307,400,401,403,404,405,500
+
+	/TODO: add a keyword which is replaced by the wordlist
+
+
 	make some scan types, to start with
 
 remember the "-" is implicit
@@ -71,9 +77,9 @@ pierre v1.0	                                        @cicada-legs
 }
 
 func parse_flags(scan *scan_config) {
-	//
-	flag.BoolVar(&scan.post, "P", false, "send HTTP request: POST by default") // TODO: make format -P GET or smth
-	flag.StringVar(&scan.wordlist_file, "w", "", "provide a wordlist")         //change later??
+
+	flag.BoolVar(&scan.post, "P", false, "send HTTP request: GET by default") // TODO: make format -P GET or smth
+	flag.StringVar(&scan.wordlist_file, "w", "", "provide a wordlist")        //change later??
 	flag.StringVar(&scan.url, "u", "", "target url")
 	flag.StringVar(&scan.extension, "x", "", "list of extensions to fuzz with: comma separated")
 	flag.IntVar(&scan.threads, "th", 1, "specify the number of threads to run on")
@@ -81,6 +87,14 @@ func parse_flags(scan *scan_config) {
 	flag.StringVar(&scan.filter_codes_string, "fs", "200,204,301,302,307,400,401,403,404,405,500", "specify which status codes to be included in output") //TODO: add more codes!!!
 
 	flag.Parse()
+
+	//if url does not contain the word PIERRE, exit
+
+	if scan.url == "" || !strings.Contains(scan.url, "PIERRE") {
+		fmt.Println("Please provide \"PIERRE\" in the URL where you want to fuzz\n Example: http://example.com/PIERRE")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 }
 
 type scan_config struct {
@@ -106,27 +120,23 @@ func (s scan_config) fuzz_scan() { //post by default
 	sc1.Split(bufio.ScanLines)
 	sc2 := bufio.NewScanner(fileread)
 	sc2.Split(bufio.ScanLines)
-	total := count_file_lines(*sc2) * len(ext_slice)
+	//total := count_file_lines(*sc2) * len(ext_slice)
 
 	if s.post {
-		// resp, err := http.Post(s.url)
 
-	} else {
-		//TODO: change this to look for a word amd replace it
-
-		for sc1.Scan() { //TODO: SHOW REDIRECTS
+		for sc1.Scan() {
 
 			for i := 0; i < len(ext_slice); i++ {
 				count++
-				loading_animation(count, total, "<", ">", 100, "=") //TODO: this isnt printing for some reason
+				// TODO: check below for errors from copilot
 
 				client := &http.Client{
 					Timeout: time.Duration(s.timeout) * time.Millisecond,
 				}
-				req, err := http.NewRequest("GET", s.url+sc1.Text()+ext_slice[i], nil) // client.Get(s.url + scanner.Text() + ext_slice[1])
+				req, err := http.NewRequest("POST", s.url+sc1.Text()+ext_slice[i], nil) // client.Get(s.url + scanner.Text() + ext_slice[1])
 
 				if err != nil {
-					handle_errors(err, "getettet")
+					handle_errors(err, "ow")
 					return
 				}
 				resp, err := client.Do(req)
@@ -139,14 +149,44 @@ func (s scan_config) fuzz_scan() { //post by default
 				defer resp.Body.Close()
 				// fmt.Println("code", resp.StatusCode)
 				if strings.Contains(s.filter_codes_string, strconv.Itoa(resp.StatusCode)) { //add to output if matching code
-					output += output + "/" + sc1.Text() + ext_slice[i] + "\t\t\t[ Status: " + strconv.Itoa(resp.StatusCode) + " Size: " + strconv.FormatInt(resp.ContentLength, 10) + " ]\n"
-					// fmt.Printf("/%s%s\t\t\t[ Status: %d  Size:%d ]\n", scanner.Text(), ext_slice[i], resp.StatusCode, resp.ContentLength)
-					//fmt.Println(output)
+					output += sc1.Text() + ext_slice[i] + "\n"
+				}
+			}
 
+		}
+
+	} else { //TODO: adapt output for subdomain fuzzing
+
+		for sc1.Scan() { //TODO: SHOW REDIRECTS
+
+			for i := 0; i < len(ext_slice); i++ {
+
+				client := &http.Client{
+					Timeout: time.Duration(s.timeout) * time.Millisecond,
+				}
+
+				req, err := http.NewRequest("GET" /*s.url+sc1.Text()+ext_slice[i]*/, strings.Replace(s.url, "PIERRE", sc1.Text()+ext_slice[i], 1), nil) // client.Get(s.url + scanner.Text() + ext_slice[1])
+
+				if err != nil {
+					handle_errors(err, "GET error: "+err.Error())
+					return
+				}
+
+				resp, err := client.Do(req)
+
+				if err != nil {
+					// fmt.Println(err) /this is for testing only
+					//connection timeout, what to output at the end of the scan?
+					continue
+				}
+
+				defer resp.Body.Close()
+
+				if strings.Contains(s.filter_codes_string, strconv.Itoa(resp.StatusCode)) { //add to output if matching code
+					output += "/" + sc1.Text() + ext_slice[i] + "\t\t\t[ Status: " + strconv.Itoa(resp.StatusCode) + " Size: " + strconv.FormatInt(resp.ContentLength, 10) + " ]\n"
 				}
 
 			}
-			fileread.Close()
 		}
 
 	}
@@ -157,29 +197,5 @@ func handle_errors(err error, msg string) {
 	if err != nil {
 		fmt.Printf(msg)
 		os.Exit(1) //change this later to be different for different errors
-	}
-}
-
-func count_file_lines(sc bufio.Scanner) int {
-	lines := 0
-	for sc.Scan() {
-		lines++
-	}
-	return lines
-}
-
-func loading_animation(iterate int, total int, prefix string, suffix string, length int, fill string) {
-	percentage := float64(iterate) / float64(total)
-	bar_progress := int(length * iterate / total)
-	end := "="
-
-	if iterate == total {
-		end = "]"
-	}
-
-	bar := strings.Repeat(fill, bar_progress) + end + strings.Repeat("-", bar_progress)
-	fmt.Printf("\r%s [%s] %f%% %s", prefix, bar, percentage, suffix)
-	if iterate == total {
-		fmt.Println()
 	}
 }
