0b641a2acac263e1cbfd01e6f04ed0747c69caac
Bo Wood <bo.wood@zxsecurity.co.nz>
2023-07-19 14:48:05 +1200

    include and exclude size both working

--- main.go
+++ main.go
@@ -226,8 +226,7 @@ func (s scan_config) fuzz_scan() { //post by default
 				//same for not matching
 				//if size_include and size_sxclude both contain a number like 400, then size_sxclude should take precedence and 400 should be excludeed (skip the if statement)
 
-				if (s.size_include == "" || ((!strings.Contains(s.size_exclude, strconv.Itoa(count_response_bytes(body_bytes)))) && strings.Contains(s.size_include, strconv.Itoa(count_response_bytes(body_bytes))))) ||
-					((!strings.Contains(s.filter_exclude, strconv.Itoa(resp.StatusCode))) && strings.Contains(s.filter_include, strconv.Itoa(resp.StatusCode))) { //add to output if matching code
+				if s.size_include == "" || ((!strings.Contains(s.size_exclude, strconv.Itoa(count_response_bytes(body_bytes)))) && strings.Contains(s.size_include, strconv.Itoa(count_response_bytes(body_bytes)))) { //add to output if matching code
 
 					if err != nil {
 						handle_errors(err, "error counting lines")
