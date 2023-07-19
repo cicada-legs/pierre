04c4533f0019d553126b1147c0c2bc7b429ef187
Bo Wood <bo.wood@zxsecurity.co.nz>
2023-07-19 16:51:13 +1200

    INCLUE/EXCLUDE WORKS EYY

--- main.go
+++ main.go
@@ -224,7 +224,7 @@ func (s scan_config) fuzz_scan() { //post by default
 				}
 
 				defer resp.Body.Close()
-				body_bytes, err := ioutil.ReadAll(resp.Body)
+				bodybytes, err := ioutil.ReadAll(resp.Body)
 
 				//if response size is not specified, include all responses!!!!
 				//TODO: CURRENTLY DOING THIS
@@ -232,14 +232,17 @@ func (s scan_config) fuzz_scan() { //post by default
 				//same for not matching
 				//if size_include and size_sxclude both contain a number like 400, then size_sxclude should take precedence and 400 should be excludeed (skip the if statement)
 
-				if s.size_include == "" || ((!strings.Contains(s.size_exclude, strconv.Itoa(count_response_bytes(body_bytes)))) && strings.Contains(s.size_include, strconv.Itoa(count_response_bytes(body_bytes)))) { //add to output if matching code
+				bodybytes_string := strconv.Itoa(count_response_bytes(bodybytes))
+				if (s.size_include == "" && s.size_exclude == "") ||
+					((strings.Contains(s.size_include, bodybytes_string) && !strings.Contains(s.size_exclude, bodybytes_string)) ||
+						s.size_exclude != "" && !strings.Contains(s.size_exclude, bodybytes_string)) { //add to output if matching code
 
 					if err != nil {
 						handle_errors(err, "error counting lines")
 					}
 
 					//FIXME: this formatting is painful, tidy it up
-					output += sc1.Text() + ext_slice[i] + "\t\t\t[ Status: " + strconv.Itoa(resp.StatusCode) + " |" + " Size: " + strconv.Itoa(count_response_bytes(body_bytes)) + " |" + " Words: " + strconv.Itoa(count_response_words(body_bytes)) + " |" + " Lines: " + strconv.Itoa(count_response_lines(body_bytes)) + " ]\n"
+					output += sc1.Text() + ext_slice[i] + "\t\t\t[ Status: " + strconv.Itoa(resp.StatusCode) + " |" + " Size: " + strconv.Itoa(count_response_bytes(bodybytes)) + " |" + " Words: " + strconv.Itoa(count_response_words(bodybytes)) + " |" + " Lines: " + strconv.Itoa(count_response_lines(bodybytes)) + " ]\n"
 
 					//DELETE BELOW LATER
 					// read and print entire response
@@ -273,19 +276,19 @@ func handle_errors(err error, msg string) {
 }
 
 // maybe get rid of these functions and just do it in the main function
-func count_response_bytes(body_bytes []byte) int {
+func count_response_bytes(bodybytes []byte) int {
 
-	return len(body_bytes)
+	return len(bodybytes)
 }
 
-func count_response_lines(body_bytes []byte) int {
+func count_response_lines(bodybytes []byte) int {
 
-	linecount := strings.Count(string(body_bytes), "\n")
+	linecount := strings.Count(string(bodybytes), "\n")
 	return linecount
 }
 
-func count_response_words(body_bytes []byte) int {
+func count_response_words(bodybytes []byte) int {
 
-	word_count := strings.Count(string(body_bytes), " ")
+	word_count := strings.Count(string(bodybytes), " ")
 	return word_count
 }
